aa4596e5593aa45e027dbd21c8192260e1caa163
Rene Reede <rene.reede@gmail.com>
2022-07-06 09:44:05 +0300

    Basics
    Variables, Prompting, if else, concat, how types blend.

---
+++ js/script.js
@@ -0,0 +1,80 @@
+"use strict";
+
+// Use strict uses most modern javascript standards
+
+// Creating variables and interchanging them
+
+/*
+let message = "Hello";
+
+alert(message);
+
+let name = "John";
+let admin = name;
+
+alert (admin);
+
+// Prompts and adding text to variables
+
+let ourPlanet = "earth";
+let currentVisitor;
+
+let results = prompt("What is your name?", '');
+
+alert(`Hi ${results}`);
+
+
+// PreFix and PostFix Incrementation
+
+
+let a = 1, b = 1;
+
+let c = ++a;
+let d = b++;
+
+
+// Different Types Computations
+
+
+"" + 1 + 0
+"" - 1 + 0
+true + false
+6 / "3"
+"2" * "3"
+4 + 5 + "px"
+"$" + 4 + 5
+"4" - 2
+"4px" - 2
+"  -9  " + 5
+"  -9  " - 5
+null + 1
+undefined + 1
+" \t \n" - 2
+
+
+// If Else..
+
+let songsOnAlbum = prompt("How many songs are on a EP?",'');
+
+if (songsOnAlbum == 6) {
+    alert("You are Right");
+} else {
+    alert("You are Wrong");
+}
+
+
+
+let ecmaScript = prompt("What is the 'offical' name of Javascript?" , '');
+
+if (ecmaScript == "ECMAscript") {
+    alert("You are right!");
+} else {
+    alert("You are wrong!");
+}
+
+let results (a + b < 4) ? ' Below ' :  "Over";
+
+*/
+
+
+
